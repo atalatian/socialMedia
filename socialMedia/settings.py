af9cc552c0ff1ad2b361a501eef25354eee65fc9
Amir Hossein Talatian <53391826+atalatian@users.noreply.github.com>
2021-10-04 11:20:57 +0330

    Update settings.py

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -63,7 +63,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'instagramsocalmediaapplication@gmail.com'
-EMAIL_HOST_PASSWORD = 'socialMediaApp'
+EMAIL_HOST_PASSWORD = 'socialmediaapp'
 
 ROOT_URLCONF = 'socialMedia.urls'
 
