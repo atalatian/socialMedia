2f79b26e4b1e271ee7ebcafa0d4d129db9422d0c
Amir Hossein Talatian <amirhoseyntalatyan@gmail.com>
2021-10-04 12:49:16 +0330

    v1.2

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -62,7 +62,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'instagramsocalmediaapplication@gmail.com'
-EMAIL_HOST_PASSWORD = 'Amirhoseyn2014'
+EMAIL_HOST_PASSWORD = 'socialmediaapp'
 
 ROOT_URLCONF = 'socialMedia.urls'
 
